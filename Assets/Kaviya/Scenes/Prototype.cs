449589ab4f96a1b406cad3fdec956cf28d4b3446
KaviyaVenkatramanan <61527151+KaviyaVenkatramanan@users.noreply.github.com>
2020-02-29 21:16:42 -0600

    Adding Scripts

---
+++ Assets/Kaviya/Scenes/Prototype.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prototype : MonoBehaviour
+{
+    public void TermButton()
+    {
+        SceneManager.LoadScene("Terminologies");
+    }
+}
+
