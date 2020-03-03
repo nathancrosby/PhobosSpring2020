431cc62b62ab2c3441fbb2649d0a1382e6c186b6
dhane <navya.murakonda@gmail.com>
2020-03-03 11:21:17 -0600

    hololens help scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public MeshRenderer m_ButtonHighlight;
+    public GameObject scenename;
+
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void OnGaze(bool hasGaze)
+    {
+        m_ButtonHighlight.enabled = hasGaze;
+    }
+
+    public void OnSelect()
+    {
+        //Debug.Log("OnSelect() called on: " + gameObject.name);
+        SceneManager.LoadScene(scenename);
+    }
+}
+
